33c2e5fbd1c7419d7ad5b53296306e0ca4437807
kent <kentlegend@163.com>
2016-10-04 22:59:26 +0800

    second commit

---
+++ src/model/Province.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author kentlee
+ *
+ */
+public class Province {
+
+	/**
+	 * 
+	 */
+	public Province() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
